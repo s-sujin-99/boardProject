768eb60720ac29e17696c2bae32a9b6f00256879
s-sujin-99 <t2684258@gmail.com>
2026-07-16 17:10:35 +0900

    [update] advice

--- SpringMybatisBoard/src/main/java/com/zeus/mapper/BoardMapper.java
+++ SpringMybatisBoard/src/main/java/com/zeus/mapper/BoardMapper.java
@@ -2,10 +2,11 @@ package com.zeus.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.zeus.domain.Board;
-
+@Mapper
 public interface BoardMapper {
     public int insert(Board board) throws Exception; 
     public Board select(Board board) throws Exception; 
